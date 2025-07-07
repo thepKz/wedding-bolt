4e1928f8902763ed811882e3b7bcbc8bc40860aa
thepKz <thepmtse181568@fpt.edu.vn>
2025-07-07 22:36:50 +0700

    Fix: add dynamic base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_REPOSITORY 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/'
 }) 
